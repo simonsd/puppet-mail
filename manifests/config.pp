19ac68b51da63ff59f35f30b25f1092ff4d1cff4
Dave Simons <dave@inuits.be>
2011-07-22 14:11:54 +0200

    add file paths and fix template refs
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,16 +2,18 @@ class mail::config {
 	file {
 		'postfix main.cf':
 			ensure => present,
+			path => '/etc/postfix/main.cf',
 			owner => root,
 			group => root,
 			mode => 0644,
-			content => Template('mail/main.cf.erb');
+			content => template('mail/main.cf.erb');
 
 		'postfix master.cf':
 			ensure => present,
+			path => '/etc/postfix/master.cf',
 			owner => root,
 			group => root,
 			mode => 0644,
-			content => Template('mail/master.cf.erb');
+			content => template('mail/master.cf.erb');
 	}
 }
